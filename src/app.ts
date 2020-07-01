d5fe3f23fa21780e8843b944f6919913ce25f3af
Jianfeng Li <jianfeng.li@pwc.com>
2020-07-01 11:53:34 +0930

    Try out Route parameters

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
-import express from 'express';
+import express, { Request, Response } from "express";
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('The sedulous hyena ate the antelope!'));
+app.get("/", (req, res) => res.send("The sedulous hyena ate the antelope!"));
+
+app.get("/variable/:arg", (req: Request, res: Response) => {
+  console.log(req.params.arg);
+  res.send(`I have seen you say this ${req.params.arg}`);
+});
 
 app.listen(port, err => {
   if (err) {
@@ -11,3 +16,4 @@ app.listen(port, err => {
   }
   return console.log(`Server is listening on http://localhost:${port}`);
 });
+
