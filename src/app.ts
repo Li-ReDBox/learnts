1af712963eef2bad7ffd7f8afd798ac6c471da04
Jianfeng Li <jianfeng.li@pwc.com>
2020-07-02 21:10:42 +0930

    Add useful tests

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 import express, { Request, Response } from "express";
 
 const app = express();
-const port = 3000;
+
+// Express configuration
+app.set("port", process.env.PORT || 3000);
 
 app.get("/", (req, res) => res.send("The sedulous hyena ate the antelope!"));
 
@@ -10,15 +12,8 @@ app.get("/variable/:arg", (req: Request, res: Response) => {
   res.send(`I have seen you say this ${req.params.arg}`);
 });
 
-app.get("/delimiter/:partOne[.-x]:partTwo(\d+)", (req: Request, res: Response) => {
+app.get("/delimiter/:partOne[.x]:partTwo", (req: Request, res: Response) => {
   res.send(`part one = ${req.params.partOne}, part two = ${req.params.partTwo}`)
-});
-
-app.listen(port, err => {
-  if (err) {
-    return console.error(err);
-  }
-  return console.log(`Server is listening on http://localhost:${port}`);
 });
 
 export default app;
--- test/app.test.ts
+++ test/app.test.ts
@@ -7,3 +7,22 @@ describe("GET /random-url", () => {
             .expect(404, done);
     });
 });
+
+describe("GET /variable", () => {
+    it("should return 200 with parameter", (done) => {
+        request(app).get("/variable/ddd")
+            .expect("I have seen you say this ddd", done);
+    });
+});
+
+describe("Delimiter is flexible", () => {
+    it("dot is allowed", (done) => {
+        request(app).get("/delimiter/1.2")
+            .expect(200, "part one = 1, part two = 2", done);
+    });
+    
+    it("x is allowed", (done) => {
+        request(app).get("/delimiter/1x2")
+            .expect(200, "part one = 1, part two = 2", done);
+    });
+});
